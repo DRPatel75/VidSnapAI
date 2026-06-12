ff5152da581948e307c9702d37bde697ecc26eaf
DRPatel75 <drpatel3009@gmail.com>
2026-06-12 12:49:22 +0530

    Implement text_to_audio and create_reel functions in generate_process.py

--- generate_process.py
+++ generate_process.py
@@ -1,12 +1,22 @@
 # This file looks for new folders in the user uploads and converts them to reel if they are not already converted 
+import os
 
 def text_to_audio(folder):
-    pass
+    print("TTA - ", folder)
 
 def create_reel(folder):
-    pass
+    print("CR - ", folder)
 
 if __name__ == "__main__":
-    folder = "588....."
-    text_to_audio(folder) # Generate the auido.mp3 from desc.txt
-    create_reel(folder) # Convert the images and audio inside the folder to a reel.  
+    with open("done.txt", "r") as f:
+        done_folders = f.readlines()
+        
+    done_folder = [f.strip() for f in done_folders]
+    folders = os.listdir("user_uploads")
+    print(folders, done_folders)
+    for folder in folders:      
+        if(folder not in done_folders):
+            text_to_audio(folder) # Generate the auido.mp3 from desc.txt
+            create_reel(folder) # Convert the images and audio inside the folder to a reel.  
+            with open("done.txt", "w") as f:
+                f.write(folder + "\n")
